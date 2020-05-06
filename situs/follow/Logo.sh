c54eb6e8d2a1c0d2cb6e922ae2ad48cc2a3b63e9
limit queen <63425451+LimitQueenProject@users.noreply.github.com>
2020-05-07 05:04:29 +0700

    Update Logo.sh

--- situs/follow/Logo.sh
+++ situs/follow/Logo.sh
@@ -1,22 +1,4 @@
-#!/bin/bash
-#clolors
-white='\e[1;37m'
-green='\e[0;32m'
-blue='\e[1;34m'
-red='\e[1;31m'
-yellow='\e[1;33m' 
-echo ""
-echo""
-
- echo -e $'\e[1;33m\e[0m\e[1;37m     ███████ ██████  ██    ██    \e[0m''\e[1;33m\e[0m\e[1;32m ██████  ██   ██ ██ ███████   \e[0m'
- echo -e $'\e[1;33m\e[0m\e[1;37m     ██   ██ ██   █  ██    ██    \e[0m''\e[1;33m\e[0m\e[1;32m ██   █  ██   ██ ██ ██        \e[0m'
- echo -e $'\e[1;33m\e[0m\e[1;37m     ███████ ██   ██ ██    ██ ███\e[0m''\e[1;33m\e[0m\e[1;32m ██████  ███████ ██ ███████   \e[0m'
- echo -e $'\e[1;33m\e[0m\e[1;37m     ██   ██ ██   ██  ██  ██     \e[0m''\e[1;33m\e[0m\e[1;32m ██      ██   ██ ██      ██   \e[0m'
- echo -e $'\e[1;33m\e[0m\e[1;37m     ██   ██ ██████    ████      \e[0m''\e[1;33m\e[0m\e[1;32m ██      ██   ██ ██ ███████   \e[0m'
-  echo""    
-  echo -e $'\e[1;33m\e[0m\e[1;33m    ██████████\e[0m''\e[1;33m\e[0m\e[1;34m██████████\e[0m''\e[1;33m\e[0m\e[1;31m██████████\e[0m' '\e[1;32m\e[0m\e[1;32m OTP BYPASS PHISHING TOOL \e[0m''\e[1;31m\e[0m\e[1;31m [v 1.0] \e[0m'                                       
-  echo ""
-   echo -e $'\e[1;33m\e[0m\e[1;33m      [ \e[0m\e[1;32m Follow on Github :- https://github.com/Ignitetch \e[0m \e[1;32m\e[0m\e[1;33m ] \e[0m'
-   echo ""
-      echo -e $'\e[1;37m\e[0m\e[1;37m  -------------------------------------------------------------------\e[0m'
-      echo -e $'\e[1;37m\e[0m\e[1;37m  --------------------------- \e[0m'
+#gosah kek gitu
+z="
+";sz='2m O';cz='m\e[';mz='   █';dz='0m\e';aBz='ojec';TBz='ps:/';Xz='""';ez='2m █';Zz='$'\''\e';rz='1;32';Tz=';33m';fBz='37m ';Ez='gree';IBz='3m\e';bBz='t \e';fz='████';CBz='e[0m';XBz='Limi';pz='█\e[';vz='S PH';DBz=';31m';EBz=' [v ';Mz='red=';cBz='[0m ';Az='whit';Vz='echo';nz='4m██';JBz='[0m\';hBz='----';Pz='m'\''';Kz='[1;3';Gz='e[0;';Qz='yell';eBz='7m\e';GBz=' \e[';ZBz='enPr';NBz=' Fol';lz='    ';gz='█  █';MBz=';32m';BBz='31m\';Nz=''\''\e[';Bz='e='\''\';Oz='1;31';yz='OOL ';uz='YPAS';gBz=' ---';Hz='32m'\''';Cz='e[1;';wz='ISHI';Yz=' -e ';jz='█ ██';KBz='33m ';HBz='0m'\''';QBz='ithu';hz='█   ';Uz=''\''';kz='\e[0';Rz='ow='\''';FBz='1.0]';WBz='com/';Jz='='\''\e';Wz=' ""';qz='0m'\'' ';tz='TP B';ABz='m'\'''\''\';bz='1;33';RBz='b :-';VBz='hub.';PBz='on G';SBz=' htt';Iz='blue';Dz='37m'\''';dBz='m ] ';Fz='n='\''\';UBz='/git';iz='██ █';Sz='\e[1';oz='m'\'''\''█';OBz='low ';az='[0m'\''';Lz='4m'\''';YBz='tQue';xz='NG T';LBz=' [ \';
+eval "$Az$Bz$Cz$Dz$z$Ez$Fz$Gz$Hz$z$Iz$Jz$Kz$Lz$z$Mz$Nz$Oz$Pz$z$Qz$Rz$Sz$Tz$Uz$z$Vz$Wz$z$Vz$Xz$z$Vz$Yz$Zz$az$Nz$bz$cz$dz$Kz$ez$fz$gz$hz$iz$jz$fz$hz$kz$Pz$z$Vz$Yz$Zz$az$Nz$bz$cz$dz$Kz$ez$hz$gz$hz$iz$jz$lz$lz$kz$Pz$z$Vz$Yz$Zz$az$Nz$bz$cz$dz$Kz$ez$fz$gz$fz$iz$jz$fz$hz$kz$Pz$z$Vz$Yz$Zz$az$Nz$bz$cz$dz$Kz$ez$hz$mz$hz$iz$hz$mz$hz$kz$Pz$z$Vz$Yz$Zz$az$Nz$bz$cz$dz$Kz$ez$hz$mz$hz$iz$jz$fz$hz$kz$Pz$z$Vz$Xz$z$Vz$Yz$Zz$az$Nz$bz$cz$dz$Kz$nz$fz$fz$kz$oz$fz$fz$pz$qz$Nz$rz$cz$dz$Kz$sz$tz$uz$vz$wz$xz$yz$kz$ABz$Cz$BBz$CBz$Sz$DBz$EBz$FBz$GBz$HBz$z$Vz$Wz$z$Vz$Yz$Zz$Kz$IBz$JBz$Cz$KBz$lz$LBz$CBz$Sz$MBz$NBz$OBz$PBz$QBz$RBz$SBz$TBz$UBz$VBz$WBz$XBz$YBz$ZBz$aBz$bBz$cBz$Sz$MBz$kz$cz$bz$dBz$kz$Pz$z$Vz$Wz$z$Vz$Yz$Zz$Kz$eBz$JBz$Cz$fBz$gBz$hBz$hBz$hBz$hBz$hBz$hBz$hBz$hBz$hBz$hBz$hBz$hBz$hBz$hBz$hBz$hBz$kz$Pz$z$Vz$Yz$Zz$Kz$eBz$JBz$Cz$fBz$gBz$hBz$hBz$hBz$hBz$hBz$hBz$GBz$HBz"
